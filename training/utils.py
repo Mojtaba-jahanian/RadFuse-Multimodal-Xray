8928fb476b12ee3c8947a21a91c3bb6bf172c89c
Mojtaba-jahanian <m.jahanian@iau.ir>
2025-04-03 10:40:12 +0330

    Update utils.py

--- training/utils.py
+++ training/utils.py
@@ -1,1 +1,17 @@
-# Training utilities
+import torch
+import numpy as np
+
+def set_seed(seed):
+    import random
+    import os
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def save_checkpoint(model, optimizer, epoch, path):
+    torch.save({
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'epoch': epoch
+    }, path)
