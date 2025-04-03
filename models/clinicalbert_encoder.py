5ff14d0f791d6a0896784afebc72a885308f7e27
Mojtaba-jahanian <m.jahanian@iau.ir>
2025-04-03 10:34:00 +0330

    Update clinicalbert_encoder.py

--- models/clinicalbert_encoder.py
+++ models/clinicalbert_encoder.py
@@ -1,1 +1,13 @@
-# ClinicalBERT encoder
+import torch.nn as nn
+from transformers import AutoModel
+
+class TextEncoder(nn.Module):
+    def __init__(self, model_name="emilyalsentzer/Bio_ClinicalBERT", embed_dim=768):
+        super(TextEncoder, self).__init__()
+        self.bert = AutoModel.from_pretrained(model_name)
+        self.projector = nn.Linear(self.bert.config.hidden_size, embed_dim)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        cls_token = outputs.last_hidden_state[:, 0, :]
+        return self.projector(cls_token)
