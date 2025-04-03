2dbf3b4ae6286ceb0c9ff41de8b30ea560403f2f
Mojtaba-jahanian <m.jahanian@iau.ir>
2025-04-03 10:34:44 +0330

    Update vit_encoder.py

--- models/vit_encoder.py
+++ models/vit_encoder.py
@@ -1,1 +1,13 @@
-# Vision Transformer encoder
+import torch.nn as nn
+from torchvision.models.vision_transformer import vit_b_16, ViT_B_16_Weights
+
+class VisionEncoder(nn.Module):
+    def __init__(self, embed_dim=768):
+        super(VisionEncoder, self).__init__()
+        self.vit = vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1)
+        self.vit.heads = nn.Identity()
+        self.projector = nn.Linear(768, embed_dim)
+
+    def forward(self, x):
+        features = self.vit(x)
+        return self.projector(features)
